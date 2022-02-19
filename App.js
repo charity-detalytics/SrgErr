665b5f36654e93fcea0af35bfccbe9125b781d9f
charity <thien.nguyen@detalytics.com>
2022-02-19 16:01:12 +0700

    Svg Err

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  View,
+} from 'react-native';
+import X from './x.svg'
+
+const App: () => Node = () => {
+  return (
+    <View>
+      <X/>
+    </View>
+  );
+};
+
+export default App;
